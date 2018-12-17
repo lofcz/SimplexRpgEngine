430a8947c026d95ac98eaac6c410c1e915ac5b50
Matěj Štágl <stagl.matej@centrum.cz>
2018-12-17 13:58:39 +0100

    added game_end

---
+++ SimplexCore/SgmlGame.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimplexCore
+{
+    public static partial class Sgml
+    {
+        public static void game_end()
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+    }
+}
