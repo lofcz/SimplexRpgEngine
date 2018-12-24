4e8595d061b7c8ab92e189bea244a9576254a602
CryoEagle <43248056+CryoEagle@users.noreply.github.com>
2018-12-25 00:44:34 +0100

    Added object ( oCryoSample )

---
+++ SimplexResources/Objects/oCryoSample.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended;
+using SimplexCore;
+using static SimplexCore.Sgml;
+
+namespace SimplexResources.Objects
+{
+    public class oCryoSample : GameObject
+    {
+        public oCryoSample()
+        {
+            EditorPath = "Actors";
+        }
+
+        public override void EvtCreate()
+        {
+
+        }
+
+        public override void EvtStep()
+        {
+
+        }
+    }
+}
