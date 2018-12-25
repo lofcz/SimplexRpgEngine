26a13df88f599ddeb724642752350045ec942d94
Matěj Štágl <stagl.matej@centrum.cz>
2018-12-25 18:26:34 +0100

    Added rest of surfaces functions

--- SimplexCore/SgmlSurface.cs
+++ SimplexCore/SgmlSurface.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
@@ -32,6 +33,78 @@ namespace SimplexCore
             GraphicsDevice.SetRenderTarget(null);
         }
 
-      
+        public static void surface_resize(ref RenderTarget2D surface, int width, int height)
+        {
+            surface = surface_create(width, width);
+        }
+
+        public static int surface_get_height(RenderTarget2D surface)
+        {
+            return surface.Height;
+        }
+
+        public static int surface_get_width(RenderTarget2D surface)
+        {
+            return surface.Width;
+        }
+
+        public static Color[] surface_get_pixels(RenderTarget2D surface)
+        {
+            Color[] rawData = new Color[surface.Width * surface.Height];
+            surface.GetData<Color>(rawData);
+            return rawData;
+        }
+
+        public static Color surface_get_pixel(RenderTarget2D surface, int x, int y)
+        {
+            Color[] c = new Color[1];
+            Rectangle extractRegion = new Rectangle(x, y, 1, 1);
+
+            surface.GetData<Color>(0, extractRegion, c, 0, 1);
+            return c[0];
+        }
+
+        public static void surface_free(RenderTarget2D surface)
+        {
+            surface.Dispose();
+        }
+
+        public enum ImageFormat
+        {
+            Png,
+            Jpg
+        }
+
+        public static void surface_save(RenderTarget2D surface, string fname, ImageFormat format = ImageFormat.Png)
+        {
+            if (format == ImageFormat.Png)
+            {
+                Stream stream = File.Create(fname + ".pmg");
+                surface.SaveAsPng(stream, surface.Width, surface.Height);
+                stream.Close();
+            }
+            else
+            {
+                Stream stream = File.Create(fname + ".jpg");
+                surface.SaveAsJpeg(stream, surface.Width, surface.Height);
+                stream.Close();
+            }
+        }
+
+        public static void surface_save_ext(RenderTarget2D surface, string fname, int width, int height, ImageFormat format = ImageFormat.Png)
+        {
+            if (format == ImageFormat.Png)
+            {
+                Stream stream = File.Create(fname + ".pmg");
+                surface.SaveAsPng(stream, width, height);
+                stream.Close();
+            }
+            else
+            {
+                Stream stream = File.Create(fname + ".jpg");
+                surface.SaveAsJpeg(stream, width, height);
+                stream.Close();
+            }
+        }
     }
 }
