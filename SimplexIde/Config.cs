3b3bb0cf3accdcb1a4c6b705be5643cfbe5aaa28
Matěj Štágl <stagl.matej@centrum.cz>
2018-12-26 21:32:36 +0100

    Added extensions system

---
+++ SimplexIde/Config.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplexIde
+{
+    public static class Config
+    {
+        public static string GameProjectName = "SimplexResources";
+        public static string GameProjectObjectsFolder = "Objects";
+
+        public static readonly string[] Extensions = {"TestExtension1"};
+    }
+}
---
+++ TestExtension1/Objects/ExtensionObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SimplexCore;
+
+namespace TestExtension1.Objects
+{
+    public class ExtensionObject : GameObject
+    {
+        public ExtensionObject()
+        {
+            EditorPath = "Extensions";
+        }
+    }
+}
