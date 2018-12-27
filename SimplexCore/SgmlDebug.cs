6f8a17f5a6def384527c259b2a7b57b2c71a94af
Matěj Štágl <stagl.matej@centrum.cz>
2018-12-27 13:25:24 +0100

    added all show_ and get_ functions

---
+++ SimplexCore/SgmlDebug.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Forms;
+using SharpDX.Direct2D1;
+
+namespace SimplexCore
+{
+    public static partial class Sgml
+    {
+        public static void show_message(string str)
+        {
+            MessageBox.Show(str);
+        }
+
+        public static void show_message_ext(string str, string caption, MessageBoxButtons buttonType)
+        {
+            MessageBox.Show(str, caption, buttonType);
+        }
+
+        public static bool show_question(string str, string caption)
+        {
+            return MessageBox.Show(str, caption, MessageBoxButtons.YesNo) == DialogResult.Yes;
+        }
+
+        public static void show_error(string str)
+        {
+            throw new Exception(str);
+        }
+
+        public static void show_message_async(string str)
+        {
+            Microsoft.Xna.Framework.Input.MessageBox.Show("", str, new []{"Ok"});
+        }
+
+        public static void show_message_async_ext(string str, string caption, string[] buttons)
+        {
+            Microsoft.Xna.Framework.Input.MessageBox.Show(caption, str, buttons);
+        }
+
+        public static double get_double(string str, string caption, string defaultValue = "")
+        {
+            double _res = Double.NaN;
+            double.TryParse(Microsoft.VisualBasic.Interaction.InputBox(str, caption, defaultValue), NumberStyles.Any, null, out _res);
+
+            return _res;
+        }
+
+        public static string get_string(string str, string caption, string defaultValue = "")
+        {
+            return Microsoft.VisualBasic.Interaction.InputBox(str, caption, defaultValue);
+        }
+
+        public static double get_int(string str, string caption, string defaultValue = "")
+        {
+            int _res = Int32.MinValue;
+            int.TryParse(Microsoft.VisualBasic.Interaction.InputBox(str, caption, defaultValue), NumberStyles.Any, null, out _res);
+
+            return _res;
+        }
+    }
+}
