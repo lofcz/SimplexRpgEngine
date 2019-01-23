f1bbd2ed94d615e62d23b98faf4e91e12b28215c
Vašek <wasekva@gmail.com>
2019-01-23 21:11:48 +0100

    New project generator

---
+++ SimplexCore/Prefabs/Global.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using SimplexCore;
+using static  SimplexCore.Sgml;
+
+namespace Prefab
+{
+    public static class Global
+    {
+        public static void DrawStart()
+        {
+
+        }
+
+        public static void DrawEnd()
+        {
+            CollisionsTree.DrawPairsDebug();
+            int n = 0;
+
+            if (currentRoom != null)
+            {
+
+
+                foreach (RoomLayer rl in currentRoom.Layers)
+                {
+                    if (rl is ObjectLayer)
+                    {
+                        n += (rl as ObjectLayer).Objects.Count;
+                    }
+                }
+            }
+
+
+            draw_text(new Vector2(200, 200), n.ToString());
+        }
+    }
+}
--- SimplexResources/Objects/oCryoSample.cs
+++ SimplexResources/Objects/oCryoSample.cs
@@ -7,6 +7,7 @@ using Microsoft.Xna.Framework.Input;
 using MonoGame.Extended;
 using SimplexCore;
 using static SimplexCore.Sgml;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace SimplexResources.Objects
 {
@@ -19,6 +20,24 @@ namespace SimplexResources.Objects
 
         public override void EvtCreate()
         {
+            /*RenderTarget2D surf = surface_create(64, 64);
+            surface_resize(ref surf, 1024, 1024);
+            surface_set_target(surf);
+            draw_circle(Position, 20, true);
+            surface_reset_target();
+            int value = surface_get_height(surf);
+            surface_get_pixels(surf);
+            Color[] col = surface_get_pixels(surf);
+            surface_free(surf);
+            surf = surface_create(64, 64);
+            surface_resize(ref surf, 1024, 1024);
+            surface_set_target(surf);
+            draw_circle(Position, 20, true);
+            surface_reset_target();
+            string a = "Surf";
+            surface_save(surf, a, ImageFormat.Jpg);
+            surface_save(surf, "Surf");
+            surface_free(surf);*/
 
         }
 
@@ -29,6 +48,16 @@ namespace SimplexResources.Objects
 
         public override void EvtDraw()
         {
+            shader_get("fuck off");
+            Vector2 pos1;
+            pos1.X = Position.X - 20;
+            pos1.Y = Position.Y - 20;
+
+            Vector2 pos2;
+            pos2.X = Position.X + 20;
+            pos2.Y = Position.Y + 20;
+
+            draw_roundrect(pos1, pos2,true);
             //draw_triangle_new(Position.X-15,Position.Y,Position.X+15,Position.Y,Position.X,Position.Y-15,true,50);
         }
     }
