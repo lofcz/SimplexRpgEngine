b41e7b9c143d13a0e34c126ec7418747dddd70f5
Matěj Štágl <stagl.matej@centrum.cz>
2019-02-04 15:57:12 +0100

    Added primes_smaller

--- SimplexCore/SGML.cs
+++ SimplexCore/SGML.cs
@@ -393,6 +393,38 @@ namespace SimplexCore
             return true;
         }
 
+        public static List<int> primes_smaller(int n)
+        {
+            List<int> k = new List<int>();
+            bool[] prime = new bool[n + 1];
+
+            for (int i = 0; i < n; i++)
+            {
+                prime[i] = true;
+            }
+
+            for (int p = 2; p * p <= n; p++)
+            {
+                if (prime[p])
+                {
+                    for (int i = p * p; i <= n; i += p)
+                    {
+                        prime[i] = false;
+                    }
+                }
+            }
+
+            for (int i = 2; i <= n; i++)
+            {
+                if (prime[i])
+                {
+                    k.Add(i);
+                }
+            }
+
+            return k;
+        }
+
         /*
          *  ------------------------------------- Trigonometry ------------------------------------
          */
