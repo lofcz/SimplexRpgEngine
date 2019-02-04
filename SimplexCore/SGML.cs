470b2bfb83287f071230928d956dc2b48da20a6a
Matěj Štágl <stagl.matej@centrum.cz>
2019-02-04 15:53:36 +0100

    Added is_prime

--- SimplexCore/SGML.cs
+++ SimplexCore/SGML.cs
@@ -374,6 +374,25 @@ namespace SimplexCore
             return int.MinValue;
         }
 
+        public static bool is_prime(int number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i += 2)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
         /*
          *  ------------------------------------- Trigonometry ------------------------------------
          */
